65c9ef2eda702461116922fd7f99a1e12d318767
Marcus Martins <vini.mark2002@gmail.com>
2023-06-11 13:46:49 -0300

    :trophy: Submission: Codewars - Reverse Words

---
+++ submissions/codewars/ReverseWords/reverse_words.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string reverse_words(string str);
+
+int main()
+{
+    string str;
+    getline(cin, str);
+
+    cout << reverse_words(str) << endl;
+
+ return 0;
+}
+
+string reverse_words(string str)
+{
+    int start_pos = 0; 
+    int string_size = str.length();
+
+    for ( int i = 0; i <= string_size; i++)
+    {
+        if ( str[i] == ' ' || i == string_size )
+        {
+            reverse( str.begin() + start_pos, str.begin() + i );
+            start_pos = i + 1;
+        }
+    }
+    
+ return str;
+}
