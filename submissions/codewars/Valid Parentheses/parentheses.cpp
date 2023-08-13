63477196e5b849fc6f8fc898c070a37693bce312
Marcus Martins <vini.mark2002@gmail.com>
2023-08-13 19:02:15 -0300

    🏆 Submission: Codewars - Does my number look big in this?

---
+++ submissions/codewars/Valid Parentheses/parentheses.cpp
@@ -0,0 +1,41 @@
+// #include <bits/stdc++.h>
+
+using namespace std;
+
+bool validParentheses(const string& str)
+{
+    if (str.empty()) return true;
+
+    // Count to keep track of open parentheses
+    int count = 0;
+
+    for (char c : str) 
+    {
+        if (c == '(')
+            count++;
+        else
+        {
+            if (c == ')') 
+            {
+                // If we encounter a ')' without matching open parentheses, the string is invalid.
+                if (count == 0)
+                    return false;
+                
+             count--;
+            }
+        }
+    }
+
+    // The string is valid if all open parentheses have been closed properly.
+ return count == 0;
+}
+
+/* int main()
+{
+    string a;
+    getline(cin, a);
+    
+    cout << validParentheses(a) << '\n';
+
+ return 0;
+} */
