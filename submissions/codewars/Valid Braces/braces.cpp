c25670bffe5fce7003cd5690095944565893f1fa
Marcus Martins <vini.mark2002@gmail.com>
2023-08-13 19:44:02 -0300

    :trophy: Submission: Codewars - Valid Braces

---
+++ submissions/codewars/Valid Braces/braces.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool valid_braces(std::string braces) 
+{
+    stack<char> stck;
+
+    for (char c : braces)
+    {
+        // If it's an opening brace, push it onto the stack
+        if (c == '(' || c == '[' || c == '{')
+            stck.push(c);
+        else
+        {
+            // If it's a closing brace and the stack is empty, return false
+            if (stck.empty()) return false;
+
+            char top = stck.top(); // Get the top element from the stack
+            stck.pop(); // Remove the top element from the stack
+
+            // Check if the current closing brace matches the most recent opening brace from the stack
+            if ((c == ')' && top != '(') ||
+                (c == ']' && top != '[') ||
+                (c == '}' && top != '{'))
+                    return false;
+        }
+    }
+
+ // If the loop completes and the stack is empty, all braces were matched properly, so return true
+ return stck.empty();
+}
+
+/* int main()
+{
+    string a;
+    getline(cin, a);
+    
+    cout << valid_braces(a) << '\n';
+
+ return 0;
+} */
